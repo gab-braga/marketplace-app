cd834df690433e360ff4669686a43f49953877a6
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-11-07 16:12:50 -0300

    feat: query orders

--- src/components/order/index.jsx
+++ src/components/order/index.jsx
@@ -1,38 +1,69 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
+import axios from "axios";
+import { ENDPOINTS } from "../../config/api";
 
-export default function Order() {
+export default function Order({ produto_id, compra_id, quantidade, total }) {
   const [product, setProduct] = useState(null);
   const [purchase, setPurchase] = useState(null);
   const [evaluate, setEvaluate] = useState(null);
   const { user } = useAuth();
 
+  function initializeData() {
+    const token = localStorage.getItem("token");
+    axios
+      .get(ENDPOINTS.getClientOrdersProduct(user?.id, produto_id), {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(({ data: { product, evaluate } }) => {
+        setProduct(product);
+        setEvaluate(evaluate);
+      });
+    axios
+      .get(ENDPOINTS.getClientOrdersPurchase(compra_id), {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => {
+        setPurchase(data);
+      });
+  }
+
+  useEffect(() => {
+    initializeData();
+  }, []);
+
+  function formatDate(str) {
+    return new Date(str).toLocaleDateString();
+  }
+
   return (
     <div className="flex items-center border border-slate-400 rounded-md overflow-hidden">
       <img
-        src=""
-        alt=""
+        src={product?.foto}
+        alt={product?.nome}
         className="w-32 xs:w-40 h-full object-cover object-center"
       />
       <div className="h-full flex-1 p-4">
-        <h3 className="text-slate-900 font-semibold">Blusa</h3>
+        <h3 className="text-slate-900 font-semibold">{product?.nome}</h3>
 
         <span className="block text-slate-500 mt-1">
           Registrado em:{" "}
           <strong className="text-slate-800 font-medium ms-2">
-            12/12/2022
+            {formatDate(purchase?.dataRegistro)}
           </strong>
         </span>
 
         <span className="block text-slate-500 mt-1">
           Quantidade:{" "}
-          <strong className="text-slate-800 font-medium ms-2">12</strong>
+          <strong className="text-slate-800 font-medium ms-2">
+            {quantidade}
+          </strong>
         </span>
 
         <div className="flex items-center justify-between gap-2">
           <span className="inline-block text-lime-600 mt-1">Entregue</span>
           <span className="inline-block text-slate-800 font-bold mt-1">
-            R$ 120
+            R$ {total}
           </span>
         </div>
 
--- src/pages/logged/orders/index.jsx
+++ src/pages/logged/orders/index.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import Order from "../../../components/order";
+import axios from "axios";
+import { ENDPOINTS } from "../../../config/api";
+import { useAuth } from "../../../context/AuthContext";
 
 export default function Orders() {
+  const [orders, setOrders] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get(ENDPOINTS.getClientOrders(user?.id), {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => {
+        setOrders(data);
+      });
+  }, []);
+
   return (
     <main className="container mx-auto flex-1 p-6 pb-16">
       <section className="w-full mt-8">
@@ -9,11 +27,9 @@ export default function Orders() {
         </h1>
 
         <div className="mt-4 w-full grid grid-cols-1 ld:grid-cols-2 xl:grid-cols-3 gap-10">
-          <Order />
-          <Order />
-          <Order />
-          <Order />
-          <Order />
+          {orders.map((order) => (
+            <Order key={order?.id} {...order} />
+          ))}
         </div>
       </section>
     </main>
