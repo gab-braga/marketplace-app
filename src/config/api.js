aef6fb0b2e4f90c84b3b88b9e74c5837b327a94e
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-11-07 11:45:48 -0300

    feat: api config endponints client and admin

--- src/config/api.js
+++ src/config/api.js
@@ -12,4 +12,43 @@ export const ENDPOINTS = {
   // auth
   postLogin: () => `${BASE_URL}/auth/login`,
   postSignup: () => `${BASE_URL}/auth/register`,
+
+  // client - profile
+  getClientProfile: (id) => `${BASE_URL}/client/profile/${id}`,
+  getClientProfile: (id) => `${BASE_URL}/client/profile/${id}/address`,
+  putClientProfile: (id) => `${BASE_URL}/client/profile/${id}`,
+  putClientProfile: (id) => `${BASE_URL}/client/profile/${id}/address`,
+  putClientPassword: (id) => `${BASE_URL}/client/profile/${id}/password`,
+  putClientPhoto: (id) => `${BASE_URL}/client/profile/${id}/photo`,
+
+  // client - cart
+  getClientCart: (id) => `${BASE_URL}/client/cart/${id}`,
+  postClientCart: (id) => `${BASE_URL}/client/cart/${id}/add`,
+  putClientCart: (id) => `${BASE_URL}/client/cart/${id}/item`,
+  postClientCartFinish: (id) => `${BASE_URL}/client/cart/${id}/finish`,
+
+  // client - orders
+  getClientOrders: (id) => `${BASE_URL}/client/orders/${id}`,
+  getClientOrdersProduct: (idU, idP) =>
+    `${BASE_URL}/client/orders/${idU}/products/${idP}`,
+  getClientOrdersPurchase: (id) => `${BASE_URL}/client/orders/purchase/${id}`,
+  getClientOrdersEvaluate: (id) => `${BASE_URL}/client/orders/${id}/evaluate`,
+
+  // admin - users
+  getAdminUsers: () => `${BASE_URL}/admin/users`,
+  getAdminUser: (id) => `${BASE_URL}/admin/users/${id}`,
+  putAdminUserEnable: (id) => `${BASE_URL}/admin/users/${id}/enable`,
+  putAdminUserDisable: (id) => `${BASE_URL}/admin/users/${id}/disable`,
+
+  // admin - products
+  getAdminProducts: () => `${BASE_URL}/admin/products`,
+  getAdminProduct: (id) => `${BASE_URL}/admin/products/${id}`,
+  postAdminProduct: () => `${BASE_URL}/admin/products`,
+  putAdminProduct: (id) => `${BASE_URL}/admin/products/${id}`,
+
+  // admin - purchases
+  getAdminPurchases: () => `${BASE_URL}/admin/purchases`,
+  getAdminPurchase: (id) => `${BASE_URL}/admin/purchases/${id}`,
+  getAdminPurchaseItems: (id) => `${BASE_URL}/admin/purchases/${id}/items`,
+  putAdminPurchaseStatus: (id) => `${BASE_URL}/admin/purchases/${id}/status`,
 };
